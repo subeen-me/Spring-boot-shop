57f6ff78e0d89c5f8d66b3f107f0b3a027801ee6
subeen-me <subeen.me@gmail.com>
2022-05-31 01:35:39 +0900

    thymeleaf 설정

---
+++ src/main/java/com/shop/dto/ItemDto.java
@@ -0,0 +1,22 @@
+package com.shop.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ItemDto {
+    private Long id;
+
+    private String itemNm;
+
+    private Integer price;
+
+    private String itemDetail;
+
+    private String sellStatCd;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
