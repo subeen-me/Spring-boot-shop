5e1f25f2cf50b8198f46060364f0c8faf48fbaee
subeen-me <subeen.me@gmail.com>
2022-06-07 03:11:37 +0900

    상품 등록 페이지 권한 테스트 추가

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,6 +31,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
                 .logoutSuccessUrl("/");
+
+        http.authorizeRequests() //시큐리티 처리에 HttpServletRequest를 이용한다
+                .mvcMatchers("/", "/members/**",
+                        "/item/**", "/images/**").permitAll() //모든 사용자가 인증(로그인)없이 경로에 접근할 수 있는 곳들 설정
+                .mvcMatchers("/admin/**").hasRole("ADMIN") //해당 계정이 ADMIN Role일때만 접근가능하도록 설정
+                .anyRequest().authenticated(); //위에서 설정한 경로 이외의 나머지 경로들은 모두 인증 요구하도록 설정
+
+        http.exceptionHandling()
+                .authenticationEntryPoint
+                        (new CustomAuthenticationEntryPoint()); //인증되지 않은 사용자가 리소스에 접근하였을 때 수행되는 핸들러 등록
     }
 
     @Bean
@@ -41,5 +52,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(memberService)
                 .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception { //static 디렉터리의 하위 파일들은 인증을 무시하도록 설정
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 }
