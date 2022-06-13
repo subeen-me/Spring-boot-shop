6de315533614a801fef90499633b2d35a39f197d
subeen-me <subeen.me@gmail.com>
2022-06-14 00:47:25 +0900

    상품 등록 ItemSerivce 추가, 테스트코드 작성

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -1,16 +1,56 @@
 package com.shop.controller;
 
 import com.shop.dto.ItemFormDto;
+import com.shop.service.ItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemService itemService;
 
     @GetMapping(value = "/admin/item/new")
     public String itemForm(Model model) {
         model.addAttribute("itemFormDto", new ItemFormDto());
         return "/item/itemForm";
     }
+
+    @PostMapping(value = "/admin/item/new")
+    public String itemNew(@Valid ItemFormDto itemFormDto, BindingResult bindingResult, Model model,
+                          @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList) {
+
+        //상품 등록 시 필수 값이 있다면 다시 상품 등록 페이지로 전환
+        if(bindingResult.hasErrors()) {
+            return "item/itemForm";
+        }
+
+        //상품 등록 시 첫 번째 이미지가 없다면 에러 메세지와 함께 상품등록 페이지로 전환.
+        //상품의 첫 번째 이미지는 메인 페이지에서 보여줄 상품 이미지로 사용하기 위해 필수 값으로 지정
+        if(itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null) {
+            model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값 입니다.");
+            return "item/itemForm";
+        }
+
+        //상품 저장 로직을 호출. 매개변수로 상품정보&상품이미지 정보를 담고 있는 itemImgFileList 를 넘겨준다.
+        try {
+            itemService.saveItem(itemFormDto, itemImgFileList);
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "상품 등록 중 에러가 발생했습니다.");
+            return "item/itemForm";
+        }
+
+        return "redirect:/"; //정상적으로 등록되었을 시 메인 페이지로 이동
+    }
+
 }
--- src/main/java/com/shop/repository/ItemImgRepository.java
+++ src/main/java/com/shop/repository/ItemImgRepository.java
@@ -4,6 +4,11 @@ import com.shop.entity.ItemImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+    //매개변수로 넘겨준 상품 아이디를 가지고, 상품 이미지 아이디의 오름차순으로 가져오는 쿼리 메소드
+    List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 }
